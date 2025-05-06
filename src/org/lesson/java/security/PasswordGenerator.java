8d4f0e8664ab0e58f464a93f3691b67b50e19e9a
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-05-06 13:18:49 +0200

    First commit

---
+++ src/org/lesson/java/security/PasswordGenerator.java
@@ -0,0 +1,26 @@
+package org.lesson.java.security;
+
+import java.util.Scanner;
+
+public class PasswordGenerator {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci il tuo Nome: ");
+        String userName = input.nextLine();
+        System.out.println("Inserisci il tuo Cognome: ");
+        String userSurname = input.nextLine();
+        System.out.println("Inserisci il tuo Colore preferito: ");
+        String userColor = input.nextLine();
+        System.out.println("Inserisci il tuo Giorno di nascita: ");
+        int userDay = input.nextInt();
+        System.out.println("Inserisci il tuo Mese di nascita: ");
+        int userMonth = input.nextInt();
+        System.out.println("Inserisci il tuo Anno di nascita: ");
+        int userYear = input.nextInt();
+
+        int sumForPass = userDay + userMonth + userYear;
+        String userPassword = userName + "-" + userSurname + "-" + userColor + "-" + sumForPass;
+        System.out.println("La tua password è: " + userPassword);
+    }
+}
